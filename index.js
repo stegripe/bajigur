4f02f1c3a2b592e4d221bb734ecfa4aa12293503
zarrttt_ <undefinedgaming01@gmail.com>
2020-08-01 09:16:24 +0700

    Add for quoted one

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ const start = async (bot) => {
         // Hello world
         if (message.body === "#hi" || message.body === "#hai" || message.body === "#halo" || message.body === "#hello") {
             bot.sendText(message.from, `Hi *${message.sender.pushname}*, if you want to make a sticker instantly, please send/quote some image attachments with caption \`\`\`#sticker\`\`\``);
-        }
+        };
         // Attachment using #sticker
         if (message.type === "image" && message.caption === "#sticker") {
             console.log(moment.tz("Asia/Jakarta").format() + " => Someone just generated a sticker: " + message.from + " ( " + message.sender.pushname + " | " + message.chat.name + " )");
             const mediaData = await wa.decryptMedia(message);
             const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString("base64")}`;
             bot.sendImageAsSticker(message.from, imageBase64);
-        }
+        };
+        // Quoted attachment using #sticker
+        if (message.quotedMsg && message.quotedMsg.type === "image" && message.body === "#sticker") {
+            console.log(moment.tz("Asia/Jakarta").format() + " => Someone just generated a sticker: " + message.from + " ( " + message.sender.pushname + " | " + message.chat.name + " )");
+            const mediaData = await wa.decryptMedia(message.quotedMsg);
+            const imageBase64 = `data:${message.quotedMsg.mimetype};base64,${mediaData.toString("base64")}`;
+            bot.sendImageAsSticker(message.from, imageBase64);
+        };
     });
 };
 
