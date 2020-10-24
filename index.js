76d08c4d4549a324cdfa231b3a4e14a49e9e3804
HYDROX <40756526+inhydrox@users.noreply.github.com>
2020-10-24 19:12:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const wa = require("@open-wa/wa-automate");
 const moment = require("moment-timezone");
-const { timezone } = require('./config')
+let { prefix, timezone } = require('./config')
 
 const start = async (bot) => {
     console.log(`[READY] ${moment.tz(timezone).format()} => The sticker bot has been booted up!`);
@@ -11,20 +11,26 @@ const start = async (bot) => {
     });
     // Message handler
     bot.onMessage(async (message) => {
+        if(!prefix) prefix = "#";
+        if(!message.body.startsWith(prefix)) return;
+        
+        let command = message.body.toLowerCase().split(" ")[0];
+        command = command.slice(prefix.length);
+        
         // Hello world
-        if (message.body === "#hi" || message.body === "#hai" || message.body === "#halo" || message.body === "#hello") {
+        if (command === "hi" || command === "hai" || command === "halo" || command === "hello") {
             console.log(`[DEBUG] [runCmd] ${moment.tz(timezone).format()} => Someone has used 'hi' command: ${message.from} ( ${message.sender.pushname} | ${message.chat.name} )`);
-            bot.sendText(message.from, `Hi *${message.sender.pushname}*, if you want to make a sticker instantly, please send/quote some image attachments with caption \`\`\`#sticker\`\`\``);
+            bot.sendText(message.from, `Hi *${message.sender.pushname}*, if you want to make a sticker instantly, please send/quote some image attachments with caption \`\`\`${prefix}sticker\`\`\``);
         };
         // Attachment using #sticker
-        if (message.type === "image" && message.caption === "#sticker") {
+        if (message.type === "image" && command === "sticker") {
             console.log(`[DEBUG] [create] ${moment.tz(timezone).format()} => Someone has just created a new sticker by attaching: ${message.from} ( ${message.sender.pushname} | ${message.chat.name} )`);
             const mediaData = await wa.decryptMedia(message);
             const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString("base64")}`;
             bot.sendImageAsSticker(message.from, imageBase64);
         };
         // Quoted attachment using #sticker
-        if (message.quotedMsg && message.quotedMsg.type === "image" && message.body === "#sticker") {
+        if (message.quotedMsg && message.quotedMsg.type === "image" && command === "sticker") {
             console.log(`[DEBUG] [create] ${moment.tz(timezone).format()} => Someone has just created a new sticker by quoting: ${message.from} ( ${message.sender.pushname} | ${message.chat.name} )`);
             const mediaData = await wa.decryptMedia(message.quotedMsg);
             const imageBase64 = `data:${message.quotedMsg.mimetype};base64,${mediaData.toString("base64")}`;
