9f05bfc469c1a4409d277b1d84185cd5ed4cc9a4
HYDROX <40756526+inhydrox@users.noreply.github.com>
2020-10-24 19:05:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-timezone: "Asia/Jakarta" //for more docs https://momentjs.com/timezone/docs/#/using-timezones/
-
+timezone: "Asia/Jakarta", //for more docs https://momentjs.com/timezone/docs/#/using-timezones/
+prefix: "#"
+  
 }
