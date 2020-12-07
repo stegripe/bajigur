ad6b9bd5d6392b749c180553d157c7dae56c1653
zarrttt <undefinedgaming01@gmail.com>
2020-12-07 11:02:55 +0700

    fix(HelpCommand): fix typos

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
+const { readdir } = require("fs");
 
 exports.run = (message, bot) => {
     let tmpMessage = [];
-    fs.readdir("./", (e, files) => {
+    readdir("./", (e, files) => {
         console.log(files);
         console.log("files");
         if (e) console.error(e);
@@ -11,6 +11,7 @@ exports.run = (message, bot) => {
             console.log("jsFile");
             const cmdFile = require(`./${jsFile}`);
             let tmpFile = {};
+            tmpFile[jsFile.replace(".js", "")] = {};
             tmpFile[jsFile.replace(".js", "")].name = cmdFile.help.name;
             tmpFile[jsFile.replace(".js", "")].description = cmdFile.help.description;
             tmpFile[jsFile.replace(".js", "")].usage = cmdFile.help.usage;
