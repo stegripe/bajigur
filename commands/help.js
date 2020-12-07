944a78c64f6bcb173595ecd80f896a0cb3523619
zarrttt <undefinedgaming01@gmail.com>
2020-12-07 11:13:48 +0700

    refactor(HelpCommand): remake

--- commands/help.js
+++ commands/help.js
@@ -1,30 +1,31 @@
 const { readdir } = require("fs");
 
-exports.run = (message, bot) => {
-    let tmpMessage = [];
-    readdir("./", (e, files) => {
-        console.log(files);
-        console.log("files");
+exports.run = (bot, message, args) => {
+    let tmpFile = {};
+    readdir("./commands/", (e, files) => {
         if (e) console.error(e);
-        files.forEach(jsFile => {
-            console.log(jsFile);
-            console.log("jsFile");
+        files.forEach((jsFile) => {
             const cmdFile = require(`./${jsFile}`);
-            let tmpFile = {};
             tmpFile[jsFile.replace(".js", "")] = {};
             tmpFile[jsFile.replace(".js", "")].name = cmdFile.help.name;
             tmpFile[jsFile.replace(".js", "")].description = cmdFile.help.description;
             tmpFile[jsFile.replace(".js", "")].usage = cmdFile.help.usage;
-            tmpMessage.push(tmpFile);
-            console.log(tmpFile);
         });
-        console.log(tmpMessage);
+
+        if (!args[0]) {
+            // prettier-ignore
+            bot.sendText(message.from, `*Available commands:* ${Object.keys(tmpFile).join(", ")}\n\n_You can run *help <command name>* to show advanced help._`);
+        } else {
+            const commandName = args[0];
+            const { name, description, usage } = require(`./${commandName}.js`).help;
+            bot.sendText(message.from, `*${name}*\n\nDescription: ${description}\nUsage: \`\`\`${usage}\`\`\``);
+        };
     });
 };
 
 exports.help = {
     name: "Help",
-    description: "Show the bot command list",
+    description: "Show this",
     usage: "help",
-    cooldown: 5
+    cooldown: 15,
 };
