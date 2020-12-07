93be9db1f6e016e986e231a99dac3649acbdbd68
zarrttt <undefinedgaming01@gmail.com>
2020-12-07 11:00:10 +0700

    fix(StickerCommand): change msg to message

--- commands/sticker.js
+++ commands/sticker.js
@@ -2,10 +2,10 @@ const { decryptMedia } = require("@open-wa/wa-decrypt");
 const uaOverride = "WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
 
 exports.run = async (message, bot) => {
-    if (message.quotedMsgObj == null || message.quotedMsgObj.type != "image") return bot.sendText(msg.from, "❎ Please caption/quote some picture!");
-    bot.sendText(msg.from, "_⌛ Please wait..._");
-    const media = await decryptMedia(msg.quotedMsgObj, uaOverride);
-    bot.sendImageAsSticker(msg.from, `data:image/jpeg;base64,${media.toString("base64")}`).then(m => bot.sendText("_✅ Done_"));
+    if (message.quotedMsgObj == null || message.quotedMsgObj.type != "image") return bot.sendText(message.from, "❎ Please caption/quote some picture!");
+    bot.sendText(message.from, "_⌛ Please wait..._");
+    const media = await decryptMedia(message.quotedMsgObj, uaOverride);
+    bot.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`).then(m => bot.sendText("_✅ Done_"));
 };
 
 exports.help = {
