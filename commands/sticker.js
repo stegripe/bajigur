bffed1a03b142e53947971d902de117c17db716b
zarrttt <undefinedgaming01@gmail.com>
2021-02-04 14:58:34 +0700

    fix(StickerCommand): don't delete this line

--- commands/sticker.js
+++ commands/sticker.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const { decryptMedia } = require("@open-wa/wa-decrypt");
 const { exec } = require("child_process");
 const fs = require("fs");
 const uaOverride = "WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
 
 exports.run = async (bot, message) => {
-
-
     if (message.isMediaObj && message.type === "image") {
         const waiting = await bot.sendText(message.from, "_⌛ Please wait..._");
         const media = await decryptMedia(message.isMediaObj, uaOverride);
@@ -29,9 +29,7 @@ exports.run = async (bot, message) => {
         fs.writeFileSync(filename, mediaData);
         try {
             exec(`gify ./assets/stickers/sticker.mp4 ./assets/stickers/output.gif --fps=30 --scale=240:240`, async (error, stdout, stderr) => {
-                const gif = fs.readFileSync("./assets/stickers/output.gif", {
-                    encoding: "base64"
-                });
+                const gif = fs.readFileSync("./assets/stickers/output.gif", { encoding: "base64" });
                 bot.sendImageAsSticker(message.from, `data:image/gif;base64, ${gif.toString("base64")}`, {
                     author: message.sender.pushname,
                     pack: "WhatsAppBot"
