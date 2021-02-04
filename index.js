02f6e67734f09e3d9fef928cb4fed2f86efa2327
zarrttt <undefinedgaming01@gmail.com>
2021-02-04 14:56:57 +0700

    chore: eslint

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+/* eslint-disable no-undef */
 const prefix = require("./config.json").prefix;
 const whatsapp = require("@open-wa/wa-automate");
 const fs = require("fs");
-let args;
-let command;
 
 const availableCommands = new Set();
 
@@ -15,29 +14,33 @@ fs.readdir("./commands", (e, files) => {
 
 whatsapp.create({
     useChrome: true,
-    headless: false,
-    chromiumArgs: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox'
-    ]
-}).then((bot) => start(bot));
+    headless: true,
+    chromiumArgs: ["--no-sandbox", "--disable-setuid-sandbox"]
+}).then(bot => start(bot));
+
+let args;
+let command;
 
 function start(bot) {
     bot.onStateChanged(async state => {
-        console.log('[Client State]', state)
-        if (state === 'CONFLICT' || state === 'UNLAUNCHED') bot.forceRefocus()
+        console.log("[Client State]", state);
+        if (state === "CONFLICT" || state === "UNLAUNCHED") bot.forceRefocus();
     });
 
     bot.onMessage(async message => {
         if (message.body.startsWith(prefix)) {
             args = message.body.slice(prefix.length).trim().split(/ +/g);
-            command = args.shift().toLowerCase()
-            sender = message.sender.pushname
+            command = args.shift().toLowerCase();
+            sender = message.sender.pushname;
         } else if (message.caption.startsWith(prefix)) {
             args = message.caption.slice(prefix.length).trim().split(/ +/g);
-            command = args.shift().toLowerCase()
-            sender = message.sender.pushname
-        } else return;
-        if (availableCommands.has(command)) { require(`./commands/${command}`).run(bot, message, args); }
+            command = args.shift().toLowerCase();
+            sender = message.sender.pushname;
+        } else {
+            return;
+        }
+        if (availableCommands.has(command)) {
+            require(`./commands/${command}`).run(bot, message, args);
+        }
     });
 }
