5b6514513f5ade00c868b9a8c2e80c4ee3553348
zarrttt <undefinedgaming01@gmail.com>
2021-02-04 07:32:52 +0700

    fix(StickerCommand): manual changes from its original PR commits

--- commands/sticker.js
+++ commands/sticker.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const { decryptMedia } = require("@open-wa/wa-decrypt");
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 const fs = require("fs");
 const uaOverride = "WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
 
@@ -11,37 +13,40 @@ exports.run = async (bot, message) => {
     if (message.isMediaObj && message.type === "image") {
         const waiting = await bot.sendText(message.from, "_⌛ Please wait..._");
         const media = await decryptMedia(message.isMediaObj, uaOverride);
-        bot.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`).then((_) => {
+        bot.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`).then(_ => {
             bot.deleteMessage(message.from, waiting);
         });
         console.log(`[DEBUG] Sticker was generated in ${Date.now() - now}ms`);
     }
 
-    else if (message.quotedMsgObj && message.quotedMsgObj.type === "image") {
+    if (message.quotedMsgObj && message.quotedMsgObj.type === "image") {
         const waiting = await bot.sendText(message.from, "_⌛ Please wait..._");
         const media = await decryptMedia(message.quotedMsgObj, uaOverride);
-        bot.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`).then((_) => {
+        bot.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`).then(_ => {
             bot.deleteMessage(message.from, waiting);
         });
         console.log(`[DEBUG] Sticker was generated in ${Date.now() - now}ms`);
     }
-    
-    else if ((message.isMedia || message.isGif) && (message.mimetype === 'video/mp4' || message.mimetype === 'image/gif') || message.type === "video") {
-        if (message.duration >= 10) return bot.reply(message.from, 'Video/gif is too long', message.id)
-        bot.reply(message.from, '_⌛ Please wait..._', message.id)
+
+    if ((message.isMedia || message.isGif) || (message.mimetype === "video/mp4" || message.mimetype === "image/gif") || message.type === "video") {
+        if (message.duration >= 10) return bot.reply(message.from, "❎ Your attachment is too big", message.id);
+        bot.reply(message.from, "_⌛ Please wait..._", message.id);
         const mediaData = await decryptMedia(message, uaOverride);
-        const filename = `./assets/stickers/sticker.${message.mimetype.split('/')[1]}`
-        fs.writeFileSync(filename, mediaData)
+        const filename = `./assets/stickers/sticker.${message.mimetype.split("/")[1]}`;
+        fs.writeFileSync(filename, mediaData);
         try {
-            await exec(`gify ./assets/stickers/sticker.mp4 ./assets/stickers/output.gif --fps=30 --scale=240:240`, async function (error, stdout, stderr) {
-                const gif = fs.readFileSync('./assets/stickers/output.gif', { encoding: "base64" })
-                bot.sendImageAsSticker(message.from, `data:image/gif;base64,${gif.toString('base64')}`, { author: message.sender.pushname, pack: 'WhatsAppBot' }) //REPLACE PACKNAME
-            })
-        } catch (error) { 
-            bot.reply(message.from, 'Error', message.id)
-    }
-    else {
-        bot.reply(message.from, 'Usage: ' + exports.help.usage+' as caption image/gif/video', message.id)
+            await exec(`gify ./assets/stickers/sticker.mp4 ./assets/stickers/output.gif --fps=30 --scale=240:240`, async (error, stdout, stderr) => {
+                const gif = fs.readFileSync("./assets/stickers/output.gif", {
+                    encoding: "base64"
+                });
+                bot.sendImageAsSticker(message.from, `data:image/gif;base64, ${gif.toString("base64")}`, {
+                    author: message.sender.pushname,
+                    pack: "WhatsAppBot"
+                });
+            });
+        } catch (error) {
+            bot.reply(message.from, "Error", message.id);
+        }
     }
 };
 
