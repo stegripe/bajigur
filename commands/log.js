1a8f550a6093fac2a425f505907f9265baa839c0
mzrtamp <40928387+mzrtamp@users.noreply.github.com>
2021-04-17 15:08:24 +0700

    fix(LogCommand): use object deconstructor when importing 'prefix'

--- commands/log.js
+++ commands/log.js
@@ -1,4 +1,4 @@
-const prefix = require("../config.json").prefix;
+const { prefix } = require("../config.json");
 
 exports.run = (bot, message) => {
     bot.sendText(message.from, "Test").then(m => {
