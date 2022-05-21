aa5e4adf8c973583ddff170261b5e9efad8a44ce
Orchit07 <orchitiadi@gmail.com>
2022-05-21 19:12:31 +0700

    chore(typings): add missing types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,11 +1,15 @@
+import { Message } from "@open-wa/wa-automate";
+
 export interface ICommandComponent {
     meta: {
         name: string;
         description: string;
         aliases: string[];
         usage: string;
+        devOnly: boolean;
+        disabled: boolean;
     };
-    execute: (...args: any) => void;
+    execute: (message: Message, ...args: any) => void;
 }
 
 export interface IListenerComponent {
