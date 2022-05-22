a2b2560ecc468fec324cb3f1e00dc3d707a0270b
Orchit07 <orchitiadi@gmail.com>
2022-05-22 21:54:01 +0700

    chore: add parseEnvValue

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -47,4 +47,20 @@ export class Utils {
     public static pathStringToURLString(path: string): string {
         return new URL(`file://${path}`).toString();
     }
+
+    public static parseEnvValue(str: string): string[] {
+        return (
+            str
+                .match(
+                    /(?<=(?:\s+|^))(?<str>['"])?(?:.*?)\k<str>(?=(?:(?:[,;])|(?:(?:\s+)?$)))/g
+                )
+                ?.filter(x => Boolean(x.trim()))
+                .map(x =>
+                    (x.startsWith("'") && x.endsWith("'")) ||
+                    (x.startsWith('"') && x.endsWith('"'))
+                        ? x.slice(1, x.length - 1)
+                        : x
+                ) ?? []
+        );
+    }
 }
