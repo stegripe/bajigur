32c0dc830ff76295caa667fa6c7e53fd77f390f6
Orchit07 <orchitiadi@gmail.com>
2022-06-24 21:50:47 +0700

    chore(utils/decorators): move decorator file

--- src/utils/decorators/ApplyMetadata.ts
+++ src/utils/decorators/ApplyMetadata.ts
@@ -1,12 +1,12 @@
-import { ICommandComponent, IListenerComponent } from "../types";
-import { WhatsAppBot } from "./WhatsAppBot";
+import { WhatsappBot } from "../../structures/WhatsappBot";
+import { ICommandComponent, IListenerComponent } from "../../types";
 
 export function ApplyMetadata<P extends ICommandComponent | IListenerComponent>(
     meta: P["meta"]
 ): any {
     return function decorate<T extends ICommandComponent | IListenerComponent>(
         target: new (...args: any[]) => T
-    ): new (whatsappbot: WhatsAppBot) => T {
+    ): new (whatsappbot: WhatsappBot) => T {
         return new Proxy(target, {
             construct: (ctx, [whatsappbot]): T => new ctx(whatsappbot, meta)
         });
