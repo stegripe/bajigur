9211ca55a8637bc5b67c0f05686de9145c468fb0
Orchit07 <orchitiadi@gmail.com>
2022-06-24 21:54:28 +0700

    fix(StickerCommand): add video support

--- src/commands/general/StickerCommand.ts
+++ src/commands/general/StickerCommand.ts
@@ -1,32 +1,32 @@
-import { ApplyMetadata } from "../../structures/ApplyMetadata.js";
-import { BaseCommand } from "../../structures/BaseCommand.js";
-import { ICommandComponent } from "../../types/index.js";
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import { decryptMedia, Message, MessageTypes } from "@open-wa/wa-automate";
+import { BaseCommand } from "../../structures/BaseCommand";
+import { ICommandComponent } from "../../types";
+import { FileTypes } from "../../types/enum";
+import { ApplyMetadata } from "../../utils/decorators/ApplyMetadata";
 
 @ApplyMetadata<ICommandComponent>({
     name: "sticker",
     description: "Generates a sticker from image.",
-    usage: "sticker",
-    aliases: [],
-    devOnly: false,
-    disabled: false
+    usage: "sticker"
 })
 export default class StickerCommand extends BaseCommand {
     public async execute(message: Message): Promise<void> {
-        const isMessageImage = message.type === MessageTypes.IMAGE;
-        const isMessageVideo = message.type === MessageTypes.VIDEO;
-        const isMessageDocument =
-            message.type === MessageTypes.DOCUMENT && ["image/png", "image/jpg", "image/jpeg", "image/webp"].includes(
-                message.mimetype!
-            );
-        const isQuotedImage = message.quotedMsg?.type === MessageTypes.IMAGE;
-        const isQuotedVideo = message.quotedMsg?.type === MessageTypes.VIDEO;
-        const isQuotedDocument =
-            message.quotedMsg?.type === MessageTypes.DOCUMENT && ["image/png", "image/jpg", "image/jpeg", "image/webp"].includes(
-                message.quotedMsg.mimetype!
-            );
+        const checkType = this.checkType(message);
 
-        if (isMessageImage || isQuotedImage) {
+        if (checkType === FileTypes.Invalid) {
+            await this.whatsappbot.client.reply(
+                message.chatId,
+                "Please send an image, video, or GIF with */sticker* caption or reply it on the file. You can also send an image as document by replying it with */sticker* too.",
+                message.id
+            );
+            return;
+        }
+
+        if (
+            checkType === FileTypes.MessageImage ||
+            checkType === FileTypes.QuoteImage
+        ) {
             const wait = (await this.whatsappbot.client.reply(
                 message.chatId,
                 "_Generating sticker..._",
@@ -35,14 +35,19 @@ export default class StickerCommand extends BaseCommand {
             await this.create(
                 message,
                 wait,
-                isMessageImage ? false : isQuotedImage,
+                checkType === FileTypes.MessageImage
+                    ? false
+                    : checkType === FileTypes.QuoteImage,
                 false
             );
 
             return undefined;
         }
 
-        if (isMessageDocument || isQuotedDocument) {
+        if (
+            checkType === FileTypes.MessageDocument ||
+            checkType === FileTypes.QuoteDocument
+        ) {
             const wait = (await this.whatsappbot.client.reply(
                 message.chatId,
                 "_Generating sticker..._",
@@ -51,14 +56,19 @@ export default class StickerCommand extends BaseCommand {
             await this.create(
                 message,
                 wait,
-                isMessageDocument ? false : isQuotedDocument,
+                checkType === FileTypes.MessageDocument
+                    ? false
+                    : checkType === FileTypes.QuoteDocument,
                 false
             );
 
             return undefined;
         }
 
-        if (isMessageVideo || isQuotedVideo) {
+        if (
+            checkType === FileTypes.MessageVideo ||
+            checkType === FileTypes.QuoteVideo
+        ) {
             if (Number(message.quotedMsg?.duration ?? message.duration) >= 11) {
                 await this.whatsappbot.client.reply(
                     message.chatId,
@@ -68,25 +78,57 @@ export default class StickerCommand extends BaseCommand {
             } else {
                 const wait = (await this.whatsappbot.client.reply(
                     message.chatId,
-                    "_Generating sticker..._ (sometimes it takes 1 - 5 minutes to process)",
+                    "_Generating sticker..._ (sometimes it takes 1 - 5 minutes to process).",
                     message.id
                 )) as Message["id"];
                 await this.create(
                     message,
                     wait,
-                    isMessageVideo ? false : isQuotedVideo,
+                    checkType === FileTypes.MessageVideo
+                        ? false
+                        : checkType === FileTypes.QuoteVideo,
                     true
                 );
             }
 
             return undefined;
         }
+    }
 
-        await this.whatsappbot.client.reply(
-            message.chatId,
-            "Please send an image, video, or GIF with */sticker* caption or reply it on the file. You can also send an image as document by replying it with */sticker* too.",
-            message.id
-        );
+    private checkType(message: Message): keyof typeof FileTypes {
+        switch (message.type) {
+            case MessageTypes.IMAGE:
+                return FileTypes.MessageImage;
+            case MessageTypes.VIDEO:
+                return FileTypes.MessageVideo;
+            case MessageTypes.DOCUMENT:
+                if (
+                    ["image", "video"].includes(message.mimetype!.split("/")[0])
+                ) {
+                    return FileTypes.MessageDocument;
+                }
+                break;
+            default:
+                switch (message.quotedMsg?.type) {
+                    case MessageTypes.IMAGE:
+                        return FileTypes.QuoteImage;
+                    case MessageTypes.VIDEO:
+                        return FileTypes.QuoteVideo;
+                    case MessageTypes.DOCUMENT:
+                        if (
+                            ["image", "video"].includes(
+                                message.quotedMsg.mimetype!.split("/")[0]
+                            )
+                        ) {
+                            return FileTypes.QuoteDocument;
+                        }
+                        break;
+                    default:
+                        return FileTypes.Invalid;
+                }
+                return FileTypes.Invalid;
+        }
+        return FileTypes.Invalid;
     }
 
     private async create(
@@ -105,21 +147,23 @@ export default class StickerCommand extends BaseCommand {
             if (isGif) {
                 await this.whatsappbot.client.sendMp4AsSticker(
                     message.chatId,
-                    media.toString("base64"), {
-                        crop: false,
-                        startTime: "00:00:00.0"
-                    }, {
+                    media.toString("base64"),
+                    {
+                        crop: false
+                    },
+                    {
                         keepScale: true,
-                        author: "Clytage Bot",
+                        author: this.whatsappbot.config.botName,
                         pack: "Sticker Creator"
                     }
                 );
             } else {
                 await this.whatsappbot.client.sendImageAsSticker(
                     message.chatId,
-                    imageBase64, {
+                    imageBase64,
+                    {
                         keepScale: true,
-                        author: "Clytage Bot",
+                        author: this.whatsappbot.config.botName,
                         pack: "Sticker Creator"
                     }
                 );
@@ -136,12 +180,15 @@ export default class StickerCommand extends BaseCommand {
             );
             await this.whatsappbot.client.reply(
                 message.chatId,
-                `An error occured when trying to create the sticker. ${
-                    isGif ? "try again with shorter video or GIF" : ""
-                }`,
+                `An error occured when trying to create the sticker, ${
+                    (e as Error).message
+                }.`,
                 message.id
             );
-            this.whatsappbot.logger.error("sticker command", e);
+            this.whatsappbot.logger.error(
+                "sticker command",
+                (e as Error).stack ?? (e as Error).message
+            );
         }
     }
 }
