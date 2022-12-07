d31fdc921a544600ee49c738c9a9abddc21f20b8
noxzym <orchitiadi@gmail.com>
2022-12-07 14:59:00 +0700

    fix(mergeDefault): eslint error

--- src/utils/functions/mergeDefault.ts
+++ src/utils/functions/mergeDefault.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-dynamic-delete, @typescript-eslint/no-unsafe-member-access */
 export function mergeDefault<T>(def: T, prov: T): T {
     const merged = { ...def, ...prov };
     const defKeys = Object.keys(def as Record<string, unknown>);
