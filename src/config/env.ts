534b55e27d5b87247f5bfebf725169f2fb0758ab
noxzym <orchitiadi@gmail.com>
2022-12-07 15:15:06 +0700

    chore(config): update default config

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ export const botName = process.env.BOT_NAME?.length
     : "Clytage Bot";
 export const prefix = process.env.PREFIX?.length ? process.env.PREFIX : "/";
 export const devs = process.env.DEVS?.split(",") ?? [];
-export const mode = (process.env.MODE as "dev" | "prod" | undefined) ?? "dev";
+export const mode = (process.env.MODE as "dev" | "prod" | undefined) ?? "prod";
 process.env.FFMPEG_PATH = resolve(
     "node_modules",
     "ffmpeg-static",
