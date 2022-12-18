845143f935eba93e5ebbbe269b0019578741c29f
mzrtamp <mozartrafikt@gmail.com>
2022-12-18 11:27:36 +0700

    fix: recover the required file

---
+++ src/structures/WhatsAppBot.ts
@@ -0,0 +1,46 @@
+import { CommandHandler, ListenerHandler } from "../utils/structures";
+import { IWhatsAppBotConfig } from "../types";
+import * as Config from "../config";
+import makeWASocket, { AuthenticationState, useMultiFileAuthState } from "@adiwajshing/baileys";
+import { resolve } from "node:path";
+import pretty from "pino-pretty";
+import pino from "pino";
+
+export class WhatsAppBot {
+    public socket: ReturnType<typeof makeWASocket> | undefined;
+    public authState:
+        | { state: AuthenticationState; saveCreds: () => Promise<void> }
+        | undefined;
+
+    public readonly config: IWhatsAppBotConfig = Config;
+
+    public readonly logger = pino(
+        {
+            level: this.config.mode === "prod" ? "info" : "debug",
+            timestamp: true
+        },
+        pretty({
+            translateTime: "SYS:yyyy-MM-dd HH:mm:ss"
+        })
+    );
+
+    public readonly commandHandler = new CommandHandler(
+        this,
+        resolve("dist/commands")
+    );
+
+    public readonly listenerHandler = new ListenerHandler(
+        this,
+        resolve("dist/events")
+    );
+
+    public async start(): Promise<void> {
+        this.authState = await useMultiFileAuthState("auth_state");
+        this.socket = makeWASocket({
+            auth: this.authState.state,
+            printQRInTerminal: true,
+            logger: this.logger
+        });
+        await this.listenerHandler.init();
+    }
+}
