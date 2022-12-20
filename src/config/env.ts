b1d76b2efed610068470576322f68aff59a3a46c
noxzym <orchitiadi@gmail.com>
2022-12-20 16:25:47 +0700

    chore(config): add STICKER_PACK variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,9 @@ import { resolve } from "path";
 export const botName = process.env.BOT_NAME?.length
     ? process.env.BOT_NAME
     : "Clytage Bot";
+export const stickerPack = process.env.STICKER_PACK?.length
+    ? process.env.STICKER_PACK
+    : "Clytage Sticker Pack";
 export const prefix = process.env.PREFIX?.length ? process.env.PREFIX : "/";
 export const devs = process.env.DEVS?.split(",") ?? [];
 export const mode = (process.env.MODE as "dev" | "prod" | undefined) ?? "prod";
