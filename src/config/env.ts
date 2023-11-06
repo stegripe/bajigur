93254fc5973e25f34d259a168336f79ead8c5fc1
noxzym <orchitiadi@gmail.com>
2023-11-06 14:53:50 +0700

    chore: change env value

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,16 +1,10 @@
 import { resolve } from "path";
 
-export const botName = process.env.BOT_NAME?.length
-    ? process.env.BOT_NAME
-    : "Clytage Bot";
+export const botName = process.env.BOT_NAME?.length ? process.env.BOT_NAME : "Clytage Bot";
 export const stickerPack = process.env.STICKER_PACK?.length
     ? process.env.STICKER_PACK
     : "Clytage Sticker Pack";
 export const prefix = process.env.PREFIX?.length ? process.env.PREFIX : "/";
 export const devs = process.env.DEVS?.split(",") ?? [];
-export const mode = (process.env.MODE as "dev" | "prod" | undefined) ?? "prod";
-process.env.FFMPEG_PATH = resolve(
-    "node_modules",
-    "ffmpeg-static",
-    "ffmpeg.exe"
-);
+export const isDev = process.env.NODE_ENV === "development";
+process.env.FFMPEG_PATH = resolve("node_modules", "ffmpeg-static", "ffmpeg.exe");
